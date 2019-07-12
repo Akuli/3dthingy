1c53567e158b55ceb43ac36ca5498f5b00efacda
Akuli <akuviljanen17@gmail.com>
2019-07-12 20:38:22 +0300

    refactor

--- hello.c
+++ hello.c
@@ -23,7 +23,7 @@ const char *argv0;
 } while(0)
 
 
-#define NUMBER_OF_FUNNY_POINTS_Z_DIRECTION 10
+#define GRID_SIZE 10
 #define VIEW_WIDTH_ANGLE 1.0f
 #define VIEW_HEIGHT_ANGLE (VIEW_WIDTH_ANGLE * SCREEN_HEIGHT / SCREEN_WIDTH)
 #define FPS 60
@@ -34,18 +34,6 @@ struct Vec3 {
 	float y;   // more means up
 	float z;   // more means back, less means forward
 };
-
-struct ScreenPoint {
-	// pixels
-	int x;     // 0 means left, more means right
-	int y;     // 0 means top, more means down
-};
-
-
-struct Player {
-	struct Vec3 loc;
-};
-
 
 static inline struct Vec3 vec3_add(struct Vec3 a, struct Vec3 b)
 {
@@ -62,6 +50,18 @@ static inline struct Vec3 vec3_sub(struct Vec3 a, struct Vec3 b)
 	return vec3_add(a, vec3_neg(b));
 }
 
+struct ScreenPoint {
+	// pixels
+	int x;     // 0 means left, more means right
+	int y;     // 0 means top, more means down
+};
+
+static bool screenpoint_isonscreen(struct ScreenPoint sp)
+{
+	return (0 <= sp.x && sp.x < SCREEN_WIDTH &&
+			0 <= sp.y && sp.y < SCREEN_HEIGHT);
+}
+
 
 // linear_map(3, 7, 0, 100, 4) == 25
 static float linear_map(float srcmin, float srcmax, float dstmin, float dstmax, float val)
@@ -70,47 +70,77 @@ static float linear_map(float srcmin, float srcmax, float dstmin, float dstmax, 
 	return dstmin + relative*(dstmax - dstmin);
 }
 
-// rel2plr should be relative to the player location
-bool get_screen_location(struct Vec3 rel2plr, struct ScreenPoint *res)
+// plrloc should be location of the player
+struct ScreenPoint vec3_to_screenpoint(struct Vec3 plrloc, struct Vec3 pnt)
 {
+	struct Vec3 rel = vec3_sub(pnt, plrloc);
+
 	// examples: 0 means right, pi/2 means forward, pi means left, -pi means left, -pi/2 means back
-	float xzangle = atan2f(-rel2plr.z, -rel2plr.x);
+	float xzangle = atan2f(-rel.z, -rel.x);
 
 	// examples: pi means down, pi/2 means forward, 0 means up
-	float yzangle = atan2f(-rel2plr.z, rel2plr.y);
+	float yzangle = atan2f(-rel.z, rel.y);
 
 	assert(0 < VIEW_WIDTH_ANGLE && VIEW_WIDTH_ANGLE < PI_FLOAT/2);
 	assert(0 < VIEW_HEIGHT_ANGLE && VIEW_HEIGHT_ANGLE < PI_FLOAT/2);
 
-	float scrx = linear_map(PI_FLOAT/2 - VIEW_WIDTH_ANGLE, PI_FLOAT/2 + VIEW_WIDTH_ANGLE, 0, SCREEN_WIDTH, xzangle);
-	float scry = linear_map(PI_FLOAT/2 - VIEW_HEIGHT_ANGLE, PI_FLOAT/2 + VIEW_HEIGHT_ANGLE, 0, SCREEN_HEIGHT, yzangle);
+	float x = linear_map(
+		PI_FLOAT/2 - VIEW_WIDTH_ANGLE, PI_FLOAT/2 + VIEW_WIDTH_ANGLE,
+		0, SCREEN_WIDTH,
+		xzangle);
+	float y = linear_map(
+		PI_FLOAT/2 - VIEW_HEIGHT_ANGLE, PI_FLOAT/2 + VIEW_HEIGHT_ANGLE,
+		0, SCREEN_HEIGHT,
+		yzangle);
 
-	res->x = (int)scrx;
-	res->y = (int)scry;
-	return (0 <= scrx && scrx < SCREEN_WIDTH &&
-			0 <= scry && scry < SCREEN_HEIGHT);
+	return (struct ScreenPoint){ (int)x, (int)y };
 }
+
+
+struct Line {
+	struct Vec3 start;
+	struct Vec3 end;
+};
+
+static void draw_line(SDL_Renderer *rnd, struct ScreenPoint sp1, struct ScreenPoint sp2)
+{
+	// FIXME: if line is partially off the screen, then draw the part that is on screen
+	if (screenpoint_isonscreen(sp1) && screenpoint_isonscreen(sp2))
+		SDL_RenderDrawLine(rnd, sp1.x, sp1.y, sp2.x, sp2.y);
+}
+
+static void line_draw(struct Vec3 plrloc, SDL_Renderer *rnd, struct Line ln)
+{
+	struct ScreenPoint sp1 = vec3_to_screenpoint(plrloc, ln.start);
+	struct ScreenPoint sp2 = vec3_to_screenpoint(plrloc, ln.end);
+	draw_line(rnd, sp1, sp2);
+}
+
+
+struct Player {
+	struct Vec3 loc;
+};
 
 void handle_key(struct Player *plr, SDL_Keysym k)
 {
 	switch(k.sym) {
 	// TODO: figure out constants for arrow keys
-	case 'a':
+	case SDLK_LEFT:
 		plr->loc.x -= PLAYER_SPEED;
 		break;
-	case 'd':
+	case SDLK_RIGHT:
 		plr->loc.x += PLAYER_SPEED;
 		break;
-	case 'w':
+	case SDLK_UP:
 		plr->loc.y += PLAYER_SPEED;
 		break;
-	case 's':
+	case SDLK_DOWN:
 		plr->loc.y -= PLAYER_SPEED;
 		break;
-	case 'e':   // forward
+	case 'f':   // forward
 		plr->loc.z -= PLAYER_SPEED;
 		break;
-	case 'q':   // back
+	case 'b':   // back
 		plr->loc.z += PLAYER_SPEED;
 		break;
 	default:
@@ -124,20 +154,34 @@ int main(int argc, char **argv)
 
 	argv0 = argv[0];
 
-	CALL_SDL(SDL_Init, SDL_INIT_VIDEO);
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		fprintf(stderr, "SDL_Init failed: %s\n", SDL_GetError());
+		abort();
+	}
 
 	SDL_Window *win;
 	SDL_Renderer *rnd;
-	CALL_SDL(SDL_CreateWindowAndRenderer, SCREEN_WIDTH, SCREEN_HEIGHT, 0, &win, &rnd);
+	if(SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &win, &rnd) < 0) {
+		fprintf(stderr, "SDL_Init failed: %s\n", SDL_GetError());
+		abort();
+	}
 
 	struct Player plr;
-	plr.loc = (struct Vec3){0};
+	plr.loc = (struct Vec3){0.0, 0.5, 0.0};
 
-	struct Vec3 points[2*NUMBER_OF_FUNNY_POINTS_Z_DIRECTION];
-	for (size_t i = 0; i < NUMBER_OF_FUNNY_POINTS_Z_DIRECTION; i++) {
-		float z = -(float)i;
-		points[2*i]     = (struct Vec3){ .x =  1.0, .y = -1.0, .z = z };
-		points[2*i + 1] = (struct Vec3){ .x = -1.0, .y = -1.0, .z = z };
+	struct Line lines[2*(2*GRID_SIZE)*(2*GRID_SIZE)];
+	size_t i = 0;
+	for (int x = -GRID_SIZE; x < GRID_SIZE; x++) {
+		for (int z = -GRID_SIZE; z < GRID_SIZE; z++) {
+			lines[i++] = (struct Line){
+				(struct Vec3){ (float)x, 0, (float)z },
+				(struct Vec3){ (float)(x+1), 0, (float)z },
+			};
+			lines[i++] = (struct Line){
+				(struct Vec3){ (float)x, 0, (float)z },
+				(struct Vec3){ (float)x, 0, (float)(z+1) },
+			};
+		}
 	}
 
 	while (true) {
@@ -147,7 +191,6 @@ int main(int argc, char **argv)
 		while (SDL_PollEvent(&evt)) {
 			switch(evt.type) {
 			case SDL_KEYDOWN:
-				printf("key down\n");
 				handle_key(&plr, evt.key.keysym);
 				break;
 
@@ -161,13 +204,10 @@ int main(int argc, char **argv)
 			}
 		}
 
-		CALL_SDL(SDL_RenderClear, rnd);
+		SDL_RenderClear(rnd);
 
-		for (size_t i = 0; i < sizeof(points)/sizeof(points[0]); i++) {
-			struct ScreenPoint sp;
-			if (get_screen_location(vec3_sub(points[i], plr.loc), &sp))
-				CALL_SDL(SDL_RenderDrawLine, rnd, sp.x, sp.y, sp.x, sp.y+10);
-		}
+		for (size_t i = 0; i < sizeof(lines)/sizeof(lines[0]); i++)
+			line_draw(plr.loc, rnd, lines[i]);
 
 		SDL_RenderPresent(rnd);
 
